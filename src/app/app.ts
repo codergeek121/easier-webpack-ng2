7ce588baf005743fb93fde65b0df3182077e4085
Niklas Häusele <niklas.haeusele@uni-ulm.de>
2016-03-09 12:17:02 +0100

    weiter aufräumenweiter aufräumen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import {Component} from 'angular2/core';
 import {FORM_PROVIDERS} from 'angular2/common';
 import {bootstrap} from "angular2/bootstrap";
-import {LicenseplateForm} from "./components/licenseplateForm";
 
 @Component({
   selector: 'app',
-  directives: [LicenseplateForm],
   template: `
-  lk
-  <plate-form></plate-form>
+  <h1>Angular-App</h1>
   `
 })
 export class App {
